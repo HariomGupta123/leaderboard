64c069b49d506b7fd418f1c905796f1f0eb176b3
HariomGupta123 <hg7716505@gmail.com>
2024-09-15 23:45:14 +0545

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+const styles=StyleSheet.create({
+  container:{},
+  topContainer:{},
+  headerTitle:{},
+  mainCard:{},
+  mainCardContainer: {},
+  mainCardImage:{},
+  mainCardRangeContainer:{},
+  mainCardTitle:{},
+  mainCardRankContainerText:{},
+  card: {},
+  cardIndex:{},
+  cardImage:{},
+  cardTitle:{},
+  cardDataContainer:{},
+  cardRankContainer:{},
+  cardRankTitle:{}
+});
